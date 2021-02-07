c0fddd7749429c2b37f8e0520be078fac40a4a42
Marko D <marko.denic@web-ideenreich.at>
2021-02-07 13:15:57 +0100

    Github repo link added to the footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,7 +1,9 @@
 <template>
   <footer class="text-center">
     <p>
-      Made by <a href="https://markodenic.com">Marko Denic</a> with <a href="https://nuxtjs.org">Nuxt.js</a>. Hosted on <a href="https://vercel.com/">Vercel</a>.
+      Made by <a href="https://markodenic.com">Marko Denic</a> with <a href="https://nuxtjs.org">Nuxt.js</a>.
+      <br>
+      Hosted on <a href="https://vercel.com/">Vercel</a>. Code on <a href="https://github.com/MarkoDenic/markdown-preview">GitHub</a>.
     </p>
   </footer>
 </template>
